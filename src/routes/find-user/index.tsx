ad58e3c7a6e51f07912f76ac586f90103fc15a20
KenParsons <102046272+KenParsons@users.noreply.github.com>
2022-11-17 12:40:28 -0500

    progress on inputs

--- src/routes/find-user/index.tsx
+++ src/routes/find-user/index.tsx
@@ -59,7 +59,7 @@ export default component$(() => {
     const displayContentEndpoint = useEndpoint("/find-user", {
         method: "get",
         skipInitialCall: true,
-        
+
     });
     const updateAccountEndpoint = useEndpoint("/find-user", { method: "get", skipInitialCall: true });
     const userSearchInput = useSignal("");
